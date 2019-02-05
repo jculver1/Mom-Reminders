ab2ac693559fd28da79cced447e6728bfe849bd8
Jessica Culver <jculver1900@gmail.com>
2019-02-05 11:26:22 -0700

    added router and navbar

--- front-endp/src/App.js
+++ front-endp/src/App.js
@@ -4,21 +4,29 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 class App extends Component {
 
+
   render() {
     return (
+      <div className="App">
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/home">Home</Link>
+                </li>
+                <li>
+                  <Link to="/reminders/">Current Reminders</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route path="/home" exact component={Home} />
+            <Route path="/reminders/" component={ReminderList} /> 
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
 
-      <Router>
-        <div className="App">
-          {/* <a 
-            className="btn-floating btn-large   waves-effect waves-light red"><i className="material-icons">
-              add</i>
-          </a> */}
-
-        </div>
-        <Router />
-
-        );
-      }
-    }
-    
-    export default App;
+export default App;
