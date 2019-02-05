cb438fdfd7c6eab3c173e54ceae20227eefc8bf0
jculver1 <jculver1900@gmail.com>
2019-02-05 14:55:11 -0700

    added home and reminderlist components

--- front-endp/src/App.js
+++ front-endp/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Navbar from './components/navbar'
+import ReminderList from './components/ReminderList'
+import Home from './components/home'
 
 class App extends Component {
 
@@ -10,16 +13,7 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-                <li>
-                  <Link to="/reminders/">Current Reminders</Link>
-                </li>
-              </ul>
-            </nav>
+            <Navbar/>
             <Route path="/home" exact component={Home} />
             <Route path="/reminders/" component={ReminderList} /> 
           </div>
--- front-endp/src/components/navbar.js
+++ front-endp/src/components/navbar.js
@@ -2,18 +2,17 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 const NavBar = () => {
+  <div>
     <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo right">Logo</a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li>
-        <Link to="/home">Home</Link>
-        </li>
-        <li><Link to="/reminders/">Current Reminders</Link>
-        </li>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/reminders/">Current Reminders</Link> </li>
       </ul>
     </div>
   </nav>
+  </div>
 }
 
 export default NavBar
