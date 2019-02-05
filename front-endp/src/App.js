34730a8e7fae87b5bf9d8864a75b718819760b90
jculver1 <jculver1900@gmail.com>
2019-02-05 10:49:27 -0700

    made adjustments to router, has two paths, one for home and the second for the reminder page.

--- front-endp/src/App.js
+++ front-endp/src/App.js
@@ -8,7 +8,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppRouter/>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/home">Home</Link>
+                </li>
+                <li>
+                  <Link to="/reminders/">Current Reminders</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route path="/home" exact component={Home} />
+            <Route path="/reminders/" component={ReminderList} /> 
+          </div>
+        </Router>
       </div>
     );
   }
