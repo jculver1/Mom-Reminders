4da80c1c3612c181efab22bfd572b3aa3ee0f5a2
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-05 11:56:38 -0700

    Created MainButton component

---
+++ front-endp/src/components/MainButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const MainButton = (props) => {
+  return (
+    <div>
+      <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+    </div>
+  )
+}
+
+export default MainButton
