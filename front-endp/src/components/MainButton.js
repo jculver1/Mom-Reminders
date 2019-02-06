6bcaabb6098e028201c2cf6b01322ab8953ef07b
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-06 08:55:23 -0700

    Changed the name to Call Mom

--- front-endp/src/components/MainButton.js
+++ front-endp/src/components/MainButton.js
@@ -5,7 +5,7 @@ import '../App.css'
 const MainButton = () => {
   return (
     <div>
-      <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+      <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">Call Mom</i></a>
     </div>
   )
 }
