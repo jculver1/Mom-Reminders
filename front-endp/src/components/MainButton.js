093fee4279801ab774fce8a280d9314694a55315
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-06 12:32:36 -0700

    Added new larger call mom button

--- front-endp/src/components/MainButton.js
+++ front-endp/src/components/MainButton.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import '../App.css'
+import image from "./CallMomButton.png"
 
 
 const MainButton = () => {
   return (
     <div>
       <a 
-        className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">Call Mom</i></a>
+        className="btn-floating btn-large waves-effect waves-light red">
+        <i className="material-icons">Call Mom</i>
+      </a>
+
+      <img src={image} alt="call mom"></img>
+      
     </div>
   )
 }
