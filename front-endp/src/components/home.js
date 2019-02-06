64e48f890c9c7113263371d840b41c5c4a41dc91
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-02-06 09:00:58 -0700

    Added input field for tasks

--- front-endp/src/components/home.js
+++ front-endp/src/components/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainButton from './MainButton.js'
 import Calendar from './calendar'
+import DropDownTasks from './DropDownTasks'
 
 const Home = (props) => {
     return(
         <div class="container">
-             <Calendar clickDate={props.clickDate}/>
+            <DropDownTasks/>
+            <Calendar clickDate={props.clickDate}/>
             <MainButton/>
         </div>
     )
